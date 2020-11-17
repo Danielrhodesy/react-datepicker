1f79b754fa29d39f9965c8d699d1baf525165dd7
Daniel Rhodes <danielrhodesnz@gmail.com>
2020-11-17 14:43:11 +1300

    Id Name

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import './react_dates_overrides.css'
 
 ReactDOM.render(
 React.createElement(App, {}, null),
-document.getElementById('react-datepicker')
+document.getElementById('react-datepicker-japanese-chinese')
 );
 
 
