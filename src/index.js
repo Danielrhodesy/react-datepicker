2056070b513c13140762b2912e9e7a965b0e5ead
Daniel Rhodes <danielrhodesnz@gmail.com>
2020-12-10 14:32:45 +1300

    changed button title

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import './react_dates_overrides.css'
 
 ReactDOM.render(
 React.createElement(App, {}, null),
-document.getElementById('react-datepicker-chinese')
+document.getElementById('react-datepicker-house')
 );
 
 
