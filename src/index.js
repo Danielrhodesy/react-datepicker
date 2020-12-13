6c1e79e15f5ab4c7a1b7e72a0220dc5e3df638f3
Daniel Rhodes <danielrhodesnz@gmail.com>
2020-12-14 08:40:56 +1300

    changed bay to bays

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import './react_dates_overrides.css'
 
 ReactDOM.render(
 React.createElement(App, {}, null),
-document.getElementById('react-datepicker-bay')
+document.getElementById('react-datepicker-bays')
 );
 
 
