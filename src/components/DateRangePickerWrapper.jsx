d06a3644669ddd5b19383a5365c7bc0755e7f05f
Daniel Rhodes <danielrhodesnz@gmail.com>
2020-12-14 15:10:27 +1300

    chnaged links

--- src/components/DateRangePickerWrapper.jsx
+++ src/components/DateRangePickerWrapper.jsx
@@ -161,7 +161,7 @@ class DateRangePickerWrapper extends React.Component {
 
     return (
       <div className="dp-container">
-        <form id="react-date-picker" class="datepicker-style" method="post" action="https://hotels.cloudbeds.com/reservas/NTJkss">
+        <form id="react-date-picker" class="datepicker-style" method="post" action="https://hotels.cloudbeds.com/reservas/mTWJZY">
             <input type="hidden" name="date_format" value="d/m/Y"/>
             <DateRangePicker
                 {...props}
