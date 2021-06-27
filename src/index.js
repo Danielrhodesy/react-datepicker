272bafb915c5c844bad6f92dfb1a58fcec9150ca
Daniel Rhodes <danielrhodesnz@gmail.com>
2021-06-27 16:32:31 +1200

    Changed residence to retreat

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import './react_dates_overrides.css'
 
 ReactDOM.render(
 React.createElement(App, {}, null),
-document.getElementById('react-datepicker-residence')
+document.getElementById('react-datepicker-retreat')
 );
 
 
